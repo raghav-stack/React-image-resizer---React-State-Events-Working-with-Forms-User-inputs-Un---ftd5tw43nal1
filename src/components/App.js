e82fc29c94c95200aaa9a893b82fb929ea712bc6
Raghav <kumarraghav71950@gmail.com>
2023-03-06 22:54:31 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
+import imgSrc from "./image.jpeg";
 
 const App = () => {
+  const [height, setHeight] = useState(320);
+  const [width, setWidth] = useState(320);
+
   return (
     <div id="main">
-
+      <form>
+        <label htmlFor="height-slider">Height Slider</label>
+        <input
+          type="range"
+          min="100"
+          max="800"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+          id="height-slider"
+        />
+        <br />
+        <label htmlFor="width-slider">Width Slider</label>
+        <input
+          type="range"
+          min="100"
+          max="800"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
+          id="width-slider"
+        />
+      </form>
+      <img
+        src={imgSrc}
+        style={{ height: `${height}px`, width: `${width}px` }}
+        id="resizable-img"
+      />
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
